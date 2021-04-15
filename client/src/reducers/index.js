45b794f4a7668a97136e9a22cabb8991052a787e
mekrishnaa <krishnakumar356808@gmail.com>
2021-04-15 10:34:49 +0530

    add gitignore file

--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import posts from './posts';
-import auth from './auth';
-
-export default combineReducers({    // it will combine our reducer functions.
-    // posts:posts,
-    posts,auth,
-});
